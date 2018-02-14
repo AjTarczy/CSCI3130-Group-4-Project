82dc830aadcb32a43eee647487409a6aeee698e8
Aj Tarczy <bleach137x@gmail.com>
2018-02-14 12:11:32 -0400

    Skeleton for the Application
    Added a skeleton for the application.

---
+++ app/src/main/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package comajtarczycsci3130_group_4_project.httpsgithub.csci3130group4coachingapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package comajtarczycsci3130_group_4_project.httpsgithub.csci3130group4coachingapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
