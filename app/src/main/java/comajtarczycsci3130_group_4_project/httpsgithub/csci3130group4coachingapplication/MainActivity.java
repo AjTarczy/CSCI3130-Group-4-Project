4ffc1b1bdbdb1122e9fb87ee3bc25cf668964e66
lizzie <ls256469@dal.ca>
2018-02-19 20:04:55 -0400

    Adding the main page

--- app/src/main/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/MainActivity.java
+++ app/src/main/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/MainActivity.java
@@ -1,13 +1,43 @@
 package comajtarczycsci3130_group_4_project.httpsgithub.csci3130group4coachingapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+{
+    String information = "test";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text = findViewById(R.id.textView);
+        text.setText(information);
+
+    }
+    public void editProfile(View view)
+    {
+        Intent showEditProfile = new Intent(this, editProfile.class);
+        startActivity(showEditProfile);
+    }
+    public void plans(View view)
+    {
+        Intent showPlans = new Intent(this, plans.class);
+        startActivity(showPlans);
+    }
+    public void manageCoach(View view)
+    {
+        Intent showManageCoach = new Intent(this, manageCoach.class);
+        startActivity(showManageCoach);
+    }
+    public void statistics(View view)
+    {
+        Intent showStatistics = new Intent(this, Statistics.class);
+        startActivity(showStatistics);
     }
 }
