266aa8e430eb025ff7ff2b327848d9eb4e6d5b0e
quaz450 <js769933@dal.ca>
2018-02-22 15:09:30 -0400

    added ui for statistics page

--- app/src/main/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/StatisticsPage.java
+++ app/src/main/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/StatisticsPage.java
@@ -6,16 +6,40 @@ package comajtarczycsci3130_group_4_project.httpsgithub.csci3130group4coachingap
 
 public class StatisticsPage {
 
-    //get total task tests
     public static int getTotalDailyTasks(){ return 3;}
     public static int getTotalWeeklyTasks(){ return 3;}
     public static int getTotalMonthlyTasks(){ return 3;}
     public static int getTotalAllTimeTasks(){ return 3;}
 
-    //get completed t
     public static int getCompletedDailyTasks(){ return 3;}
     public static int getCompletedWeeklyTasks(){ return 3;}
     public static int getCompletedMonthlyTasks(){ return 3;}
     public static int getCompletedAllTimeTasks(){ return 3;}
 
+    public static int getDailyCalsBurned(){ return 3;}
+    public static int getWeeklyCalsBurned(){ return 3;}
+    public static int getMonthlyCalsBurned(){ return 3;}
+    public static int getAllTimeCalsBurned(){ return 3;}
+
+    public static double calculateCalsBurned(String sex, int age, double weight, int time){
+        double ageConst=0, weightConst=0, heartRateConst=0, timeConst=0, constant=0;
+
+        if(sex.equalsIgnoreCase("male")) {
+            ageConst = 0.2017;
+            weightConst = 0.09036;
+            heartRateConst = 120*0.6309;
+            timeConst = 4.184;
+            constant = 55.0969;
+        }
+
+        else if(sex.equalsIgnoreCase("female")){
+            ageConst = 0.074;
+            weightConst = 0.05741;
+            heartRateConst = 120*0.4472;
+            timeConst = 4.184;
+            constant = 20.4022;
+        }
+
+        double result = (age*ageConst + weight*weightConst + heartRateConst - constant)*(time/timeConst);
+    }
 }
--- app/src/test/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/StatisticsUnitTest.java
+++ app/src/test/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/StatisticsUnitTest.java
@@ -9,45 +9,60 @@ import static org.junit.Assert.*;
 
 public class StatisticsUnitTest {
 
+    //get total task tests
     @Test
     public void dailyTotalTasks(){
         assertEquals(3, StatisticsPage.getTotalDailyTasks());
     }
-
     @Test
     public void weeklyTotalTasks(){
         assertEquals(3, StatisticsPage.getTotalWeeklyTasks());
     }
-
     @Test
     public void monthlyTotalTasks(){
         assertEquals(3, StatisticsPage.getTotalMonthlyTasks());
     }
-
     @Test
     public void allTimeTotalTasks(){
         assertEquals(3, StatisticsPage.getTotalAllTimeTasks());
     }
 
+    //get completed task tests
     @Test
     public void dailyCompletedTasks(){
         assertEquals(3, StatisticsPage.getCompletedDailyTasks());
     }
-
     @Test
     public void weeklyCompletedTasks(){
         assertEquals(3, StatisticsPage.getCompletedWeeklyTasks());
     }
-
     @Test
     public void monthlyCompletedTasks(){
         assertEquals(3, StatisticsPage.getCompletedMonthlyTasks());
     }
-
     @Test
     public void allTimeCompletedTasks(){
         assertEquals(3, StatisticsPage.getCompletedAllTimeTasks());
     }
 
+    //calories burned tests
+    @Test
+    public void dailyCalsBurned(){
+        assertEquals(3, StatisticsPage.getDailyCalsBurned());
+    }
+    @Test
+    public void weeklyCalsBurned(){
+        assertEquals(3, StatisticsPage.getWeeklyCalsBurned());
+    }
+    @Test
+    public void monthlyCalsBurned(){
+        assertEquals(3, StatisticsPage.getMonthlyCalsBurned());
+    }
+    @Test
+    public void allTimeCalsBurned(){
+        assertEquals(3, StatisticsPage.getAllTimeCalsBurned());
+    }
+
+ 
 
 }
