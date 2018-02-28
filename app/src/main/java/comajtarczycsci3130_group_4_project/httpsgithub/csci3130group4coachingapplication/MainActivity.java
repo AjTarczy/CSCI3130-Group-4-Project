37e1a5f3d672eecaf17b01ad2c17728f03bf6f2b
robertn <robertnickerson@gmail.com>
2018-02-28 13:26:10 -0400

    Revert "Merge branch 'master' into US4"
    This reverts commit 1e37f8ea12ba02891b1c3b1cc4468e4a5b30b695, reversing
    changes made to d22c96a596c436e273cfc438b8e5fbc82e39e84e.

--- app/src/main/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/MainActivity.java
+++ app/src/main/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/MainActivity.java
@@ -3,28 +3,13 @@ package comajtarczycsci3130_group_4_project.httpsgithub.csci3130group4coachingap
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity
 {
     String information = "test";
-
-    private EditText mUsernameView;
-    private EditText mPasswordView;
-
-    //create database reference
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference usersRef = database.getReference("users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,76 +18,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         TextView text = findViewById(R.id.textView);
         text.setText(information);
-        
-        Button mLoginButton = findViewById(R.id.login_button);
 
-        //set event listener for login button press
-        mLoginButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                mUsernameView = (EditText) findViewById(R.id.main_username_input);
-                mPasswordView = (EditText) findViewById(R.id.main_password);
-
-
-                //create database event listener to query database for user login info
-                usersRef.addListenerForSingleValueEvent(new ValueEventListener()
-                {
-                    @Override
-                    public void onDataChange(DataSnapshot data)
-                    {
-
-                        String username = mUsernameView.getText().toString();
-                        String password = mPasswordView.getText().toString();
-
-                        //if username is blank
-                        if (username.equals(""))
-                        {
-                            return;
-                        }
-
-                            //if username is in the database
-                            if (data.child(username).exists())
-                            {
-
-                                //if password in database matches entered password
-                                if (data.child(username).child("password").getValue().equals(password))
-                                {
-                                    login();
-                                }
-
-                            }
-
-                            //if user enters an unrecognized username, go to registration page
-                            else
-                            {
-                                register();
-                            }
-                        }
-
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError)
-                    {
-
-                    }
-                });
-
-            }
-        });
     }
-
-    private void login()
-    {
-        startActivity(new Intent(MainActivity.this, LoggedInActivity.class));
-    }
-
-    private void register()
-    {
-        startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
-    }
-
     public void editProfile(View view)
     {
         Intent showEditProfile = new Intent(this, editProfile.class);
@@ -123,7 +40,4 @@ public class MainActivity extends AppCompatActivity
         Intent showStatistics = new Intent(this, Statistics.class);
         startActivity(showStatistics);
     }
-        
-
-
 }
