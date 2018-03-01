21760332e02d47d3ee3cccfb66383f16dfcc51ee
quaz450 <js769933@dal.ca>
2018-02-28 20:39:55 -0400

    Revert "Revert "start of espresso test""
    This reverts commit 4675a8c6412d7bf300b559bcf5559b3416e43e49.

---
+++ app/src/androidTest/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/StatisticsPageEspressoTest.java
@@ -0,0 +1,33 @@
+package comajtarczycsci3130_group_4_project.httpsgithub.csci3130group4coachingapplication;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+/**
+ * Created by Jessi on 2018-02-28.
+ */
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class StatisticsPageEspressoTest {
+
+    @Rule
+    public ActivityTestRule<StatisticsPage> statisticsPageRule = new ActivityTestRule<>(StatisticsPage.class);
+
+    @Test
+    public void checkTodayStats(){}
+
+    @Test
+    public void checkThisWeekStats(){}
+
+    @Test
+    public void checkThisMonthStats(){}
+
+    @Test
+    public void checkTodayStats(){}
+
+}
