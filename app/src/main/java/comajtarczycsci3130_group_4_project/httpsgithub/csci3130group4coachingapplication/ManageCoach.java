f791a53aab6545c5fd8896142c53b6927d626bbb
Gashin <g.gashin@dal.ca>
2018-03-16 18:12:09 -0300

    us11

--- app/src/main/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/ManageCoach.java
+++ app/src/main/java/comajtarczycsci3130_group_4_project/httpsgithub/csci3130group4coachingapplication/ManageCoach.java
@@ -3,11 +3,11 @@ package comajtarczycsci3130_group_4_project.httpsgithub.csci3130group4coachingap
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class manageCoach extends AppCompatActivity {
+public class ManageCoach extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.manage_coach);
+        setContentView(R.layout.manage__coach);
     }
 }
